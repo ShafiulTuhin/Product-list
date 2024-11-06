1b1b99127d385ef411ccfcf9ef46aeac7b9e8a19
ShafiulTuhin <tuhin6488@gmail.com>
2024-11-06 06:41:18 +0600

    Updated raw data

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Footer from "./Footer";
+import Header from "./Header";
+
+import ProductCollections from "./ProductCollections";
+import ProductBoard from "./products/ProductBoard";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <ProductCollections />
+      <ProductBoard />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
