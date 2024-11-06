f2887cc8d5740acf0958edd03aa33f5d1afb8e4e
ShafiulTuhin <tuhin6488@gmail.com>
2024-11-06 09:58:36 +0600

    fetched Product Data

--- src/products/ProductBoard.jsx
+++ src/products/ProductBoard.jsx
@@ -1,6 +1,12 @@
-import Shirt2 from "../assets/products/shirt-2.png";
+import ProductCart from "./ProductCart";
+import ProductsDetails from "./ProductsDetails";
+import Search from "./Search";
+
+import { useProduct } from "./hooks";
 
 const ProductBoard = () => {
+  const { productData, error, loading } = useProduct();
+  console.log(productData, error, loading);
   return (
     <div>
       <div className="pt-16 sm:pt-24 lg:pt-40">
@@ -135,55 +141,8 @@ const ProductBoard = () => {
             </div>
 
             <div className="flex gap-2 items-center">
-              <div className="flex flex-1 items-center px-3.5 py-2 text-gray-400 group hover:ring-1 hover:ring-gray-300 focus-within:!ring-2 ring-inset focus-within:!ring-teal-500 rounded-md">
-                <svg
-                  className="mr-2 h-5 w-5 stroke-slate-400"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                  ></path>
-                </svg>
-                <input
-                  className="block w-full appearance-none bg-transparent text-base text-gray-700 placeholder:text-gray-400 focus:outline-none placeholder:text-sm sm:text-sm sm:leading-6"
-                  placeholder="Find anything..."
-                  aria-label="Search components"
-                  id="headlessui-combobox-input-:r5n:"
-                  role="combobox"
-                  type="text"
-                  aria-expanded="false"
-                  aria-autocomplete="list"
-                  value=""
-                  //   style="caret-color: rgb(107, 114, 128)"
-                />
-              </div>
-
-              <div className="flow-root">
-                <a href="#" className="group -m-2 flex items-center p-2">
-                  <svg
-                    className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-                    />
-                  </svg>
-                  <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
-                    0
-                  </span>
-                  <span className="sr-only">items in cart, view bag</span>
-                </a>
-              </div>
+              <Search />
+              <ProductCart />
             </div>
           </div>
         </div>
@@ -192,195 +151,7 @@ const ProductBoard = () => {
           <div className="bg-white">
             <div className="mx-auto max-w-2xl px-4 py-4 sm:px-6 lg:max-w-7xl lg:px-8">
               <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                <div className="relative">
-                  <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md lg:aspect-none lg:h-80">
-                    <img
-                      src={Shirt2}
-                      alt="Front of men&#039;s Basic Tee in black."
-                      className="h-full w-full object-cover object-top lg:h-full lg:w-full p-4 bg-gray-100"
-                    />
-                  </div>
-                  <div className="mt-4 px-3 pb-4">
-                    <div>
-                      <h3 className="text-sm text-gray-700">
-                        Green grandad Shirt
-                      </h3>
-                      <p className="mt-1 text-sm text-gray-500">
-                        Men's clothing
-                      </p>
-                    </div>
-                    <p className="text-sm font-medium text-gray-900">$35</p>
-                  </div>
-
-                  <div className="cursor-pointer rounded-md bg-white text-[0.8125rem] font-medium leading-5 text-slate-700 ring-1 ring-slate-700/10  hover:ring-1 ring-slate-700/10 hover:bg-slate-50 hover:text-slate-900 items-center text-center mb-3 mx-3 flex-1">
-                    <div className="flex px-3 py-2 justify-center">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="currentColor"
-                        className="mr-2.5 h-5 w-5 flex-none stroke-slate-400"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-                        />
-                      </svg>
-                      Add To Cart
-                    </div>
-                  </div>
-                </div>
-                {/* <div className="relative">
-                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md lg:aspect-none lg:h-80">
-                  <img
-                    src="./assets/products/shirt-3.png"
-                    alt="Front of men&#039;s Basic Tee in black."
-                    className="h-full w-full object-cover object-top lg:h-full lg:w-full bg-gray-100"
-                  />
-                </div>
-                <div className="mt-4 px-3 pb-4">
-                  <div>
-                    <h3 className="text-sm text-gray-700">
-                      {" "}
-                      Gray short sleeve Polo Shirt{" "}
-                    </h3>
-                    <p className="mt-1 text-sm text-gray-500">
-                      Men's clothing
-                    </p>
-                  </div>
-                  <p className="text-sm font-medium text-gray-900">$35</p>
-                </div>
-
-                <div className="cursor-pointer rounded-md bg-white text-[0.8125rem] font-medium leading-5 text-slate-700 ring-1 ring-slate-700/10  hover:ring-1 ring-slate-700/10 hover:bg-slate-50 hover:text-slate-900 items-center text-center mb-3 mx-3 flex-1">
-                  <div className="flex px-3 py-2 justify-center">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      className="mr-2.5 h-5 w-5 flex-none stroke-slate-400"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-                      />
-                    </svg>
-                    Add To Cart
-                  </div>
-                </div>
-              </div>
-
-              <div className="relative">
-                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md lg:aspect-none lg:h-80">
-                  <img
-                    src="./assets/products/shirt-4.png"
-                    alt="Front of men&#039;s Basic Tee in black."
-                    className="h-full w-full object-cover object-top lg:h-full lg:w-full  bg-gray-100"
-                  />
-                </div>
-                <div className="mt-4 px-3 pb-4">
-                  <div>
-                    <h3 className="text-sm text-gray-700">
-                      {" "}
-                      Orange pique dress collar polo{" "}
-                    </h3>
-                    <p className="mt-1 text-sm text-gray-500">
-                      Men's clothing
-                    </p>
-                  </div>
-                  <p className="text-sm font-medium text-gray-900">$35</p>
-                </div>
-
-                <div className="cursor-pointer rounded-md bg-white text-[0.8125rem] font-medium leading-5 text-slate-700 ring-1 ring-slate-700/10  hover:ring-1 ring-slate-700/10 hover:bg-slate-50 hover:text-slate-900 items-center text-center mb-3 mx-3 flex-1">
-                  <div className="flex px-3 py-2 justify-center">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      className="mr-2.5 h-5 w-5 flex-none stroke-slate-400"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-                      />
-                    </svg>
-                    Add To Cart
-                  </div>
-                </div>
-              </div>
-
-              <div className="relative">
-                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md lg:aspect-none lg:h-80">
-                  <img
-                    src="./assets/products/shirt-1.png"
-                    alt="Front of men&#039;s Basic Tee in black."
-                    className="h-full w-full object-cover object-top lg:h-full lg:w-full  bg-gray-100"
-                  />
-                </div>
-                <div className="mt-4 px-3 pb-4">
-                  <div>
-                    <h3 className="text-sm text-gray-700">
-                      {" "}
-                      Black tencel long sleeved polo{" "}
-                    </h3>
-                    <p className="mt-1 text-sm text-gray-500">
-                      Men's clothing
-                    </p>
-                  </div>
-                  <p className="text-sm font-medium text-gray-900">$35</p>
-                </div>
-
-                <div className="cursor-pointer rounded-md bg-white text-[0.8125rem] font-medium leading-5 text-slate-700 ring-1 ring-slate-700/10  hover:ring-1 hover:ring-slate-700/20 hover:bg-slate-50 hover:text-slate-900 items-center text-center mb-3 mx-3 flex-1">
-                  <div className="flex px-3 py-2 justify-center">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      className="mr-2.5 h-5 w-5 flex-none stroke-slate-400"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-                      />
-                    </svg>
-                    Add To Cart
-                  </div>
-                </div>
-              </div> */}
-
-                {/* <div className="bg-white p-4 rounded-lg shadow animate-pulse">
-                <div className="bg-gray-300 h-48 rounded-md mb-4"></div>
-
-                <div className="h-4 bg-gray-300 rounded w-3/4 mb-2"></div>
-
-                <div className="h-3 bg-gray-300 rounded w-1/2 mb-2"></div>
-
-                <div className="h-4 bg-gray-300 rounded w-1/4 mb-4"></div>
-
-                <div className="h-10 bg-gray-300 rounded"></div>
-              </div>
-
-              <div className="bg-white p-4 rounded-lg shadow animate-pulse">
-                <div className="bg-gray-300 h-48 rounded-md mb-4"></div>
-
-                <div className="h-4 bg-gray-300 rounded w-3/4 mb-2"></div>
-
-                <div className="h-3 bg-gray-300 rounded w-1/2 mb-2"></div>
-
-                <div className="h-4 bg-gray-300 rounded w-1/4 mb-4"></div>
-
-                <div className="h-10 bg-gray-300 rounded"></div>
-              </div> */}
+                <ProductsDetails />
               </div>
             </div>
           </div>
---
+++ src/products/hooks/useProduct.js
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+
+const useProduct = () => {
+  const [productData, setProductData] = useState([]);
+  const [loading, setLoading] = useState({
+    state: false,
+    message: "",
+  });
+
+  const [error, setError] = useState(null);
+
+  const fetchProductData = async () => {
+    try {
+      setLoading({
+        ...loading,
+        state: true,
+        message: "Fetching Product Data...",
+      });
+      const response = await fetch(`https://fakestoreapi.com/products`);
+      if (!response.ok) {
+        const errorMessage = `Fetching Product data failed: ${response.status}`;
+        throw new Error(errorMessage);
+      }
+      const data = await response.json();
+
+      console.log(data);
+
+      setProductData(data);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading({
+        ...loading,
+        state: false,
+        message: "Fetching Product Data...",
+      });
+    }
+  };
+
+  useEffect(() => {
+    setLoading({
+      ...loading,
+      state: true,
+      message: "Finding Products...",
+    });
+
+    fetchProductData();
+  }, []);
+
+  return { productData, error, loading };
+};
+
+export default useProduct;
